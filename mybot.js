3dde5c3a12718a9b591f83b069a883c1da1eeab9
Tynoxs <tynoxs@gmail.com>
2018-10-14 20:20:15 +0200

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -11,10 +11,7 @@ client.on('message', message => {
     if (!message.content.startsWith(prefix)) return;
     let command = message.content.split(" ")[0];
     command = command.slice(prefix.length);
-    
-    let args = message.content.split(" ").slice(1);
-    var argresult = args.join('');
-    
+
     if (command === "help") {
     message.channel.sendMessage('```Unavailable```');
     } else    
