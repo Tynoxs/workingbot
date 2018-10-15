85e45635ab966e1c9d4966de91b5e9251bc09db6
Tynoxs <tynoxs@gmail.com>
2018-10-15 20:14:36 +0200

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -31,6 +31,7 @@ client.on('message', message => {
         if(!message.member.roles.has(adminRole.id)) {
             return message.channel.sendMessage("You are not authorised to use this command!");
         } 
+        message.channel.sendMessage("*User has been reported!*")
         client.channels.get('501251380833550336').sendMessage(`**Staff Alert:** ${args.join(" ")}`);
     }
   
@@ -40,7 +41,7 @@ client.on('message', message => {
         if(!message.member.roles.has(adminRole.id)) {
             return message.channel.sendMessage("You are not authorised to use this command!");
         } 
-        client.channels.get('501450922053074984').sendMessage(`**Report:** ${args.join(" ")}`);
+        client.channels.get('501450922053074984').sendMessage(`**Report from ${member.user.username}:** ${args.join(" ")}`);
     }
   
    if (command === "kick") {
