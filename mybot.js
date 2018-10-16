1064a08ec47acb1fbc045f7231cdd1549d9ffb58
Tynoxs <tynoxs@gmail.com>
2018-10-17 00:41:22 +0200

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -135,38 +135,10 @@ client.on('message', message => {
             return message.channel.sendMessage("I don't have the permission (KICK_MEMBER) to do this.");
         }
         kickMember.kick().then(member => {
-          
-        client.channels.get('501801149071097866').sendMessage({embed: {
-        color: 15105570,
-        author: {
-        name: "Kicked User",
-        icon_url: message.guild.iconURL
-        },
-        description: "A User has been kicked from TheHUB",
-        fields: [{
-        name: "Kicked User",
-        value: (`${kUser} with ID ${kUser.id}`)
-        },
-        {
-        name: "Kicked By",
-        value: (`<@${message.author.id}> with ID ${message.author.id}`)
-        },
-        {
-        name: "Kicked At",
-        value: (`${message.createdAt}`)
-        },
-        {
-        name: "Reason",
-        value: "Unavailable"
-        }       
-        ],
-        timestamp: new Date(),
-        footer: {
-         icon_url: message.guild.iconURL,
-        text: "© TheHUB"
+           message.channel.sendMessage(`*${member.user.username} was kicked!*`).catch(console.error);
+        }).catch(console.error);
         }
-        }
-        });
+        
     
     //COMMAND - BAN --- /ban @user --- bans a user from the server
     if (command === "ban") {
@@ -186,38 +158,8 @@ client.on('message', message => {
             return message.channel.sendMessage("I don't have the permission (BAN_MEMBER) to do this.");
         }
         banMember.ban().then(member => {
-          
-          client.channels.get('501801149071097866').sendMessage({embed: {
-        color: 15158332,
-        author: {
-        name: "Banned User",
-        icon_url: message.guild.iconURL
-        },
-        description: "A User has been banned from TheHUB",
-        fields: [{
-        name: "Banned User",
-        value: (`${bUser} with ID ${bUser.id}`)
-        },
-        {
-        name: "Banned By",
-        value: (`<@${message.author.id}> with ID ${message.author.id}`)
-        },
-        {
-        name: "Banned At",
-        value: (`${message.createdAt}`)
-        },
-        {
-        name: "Reason",
-        value: "Unavailable"
-        }       
-        ],
-        timestamp: new Date(),
-        footer: {
-         icon_url: message.guild.iconURL,
-        text: "© TheHUB"
+           message.channel.sendMessage(`*${member.user.username} was banned!*`).catch(console.error);
+        }).catch(console.error);
         }
-        }
-        });
-
 
 client.login(process.env.BOT_TOKEN);
