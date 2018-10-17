81da2e5ef86f4badf05528927f6d023024b8856f
Tynoxs <tynoxs@gmail.com>
2018-10-17 22:23:14 +0200

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -85,7 +85,7 @@ client.on('message', message => {
     }
   
     //repond to hello
-    if(message.content === "hello") {
+    if(command === "hello") {
       if (message.author.bot) return;
       if (!message.author.bot) {
       function random_greeting(greet)
