916833aaa27f27ccee442b652a842cb930ed8194
Tynoxs <tynoxs@gmail.com>
2018-10-28 11:59:09 +0100

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -47,6 +47,18 @@ const prefix = '/';
 	//                                                      //
 	//////////////////////////////////////////////////////////
 		
+	//JOIN////////////////////////////////////////////////////
+	if (message.member.voiceChannel) {
+      		message.member.voiceChannel.join()
+        	.then(connection => { // Connection is an instance of VoiceConnection
+          //message.reply('I have successfully connected to the channel!');
+        })
+        .catch(console.log);
+    	
+	} else {
+      		message.reply('You need to join a voice channel first!');
+    	}
+		
 	//IMAGE REMOVAL///////////////////////////////////////////
 	
 	
